9a52f8eeb8d222bd904b3bb815e3d15ae5cfabad
Shaikh Kingshuk <shaikhkingshuk@gmail.com>
2025-10-19 23:14:28 +0600

    date added using date-fns npm package

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import headerLogo from "../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <img className="h-[60px]" src={headerLogo}></img>
       <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p className="text-accent font-semibold">
+        {format(new Date(), "EEEE, LLLL d, yyyy")}{" "}
+      </p>
     </div>
   );
 };
