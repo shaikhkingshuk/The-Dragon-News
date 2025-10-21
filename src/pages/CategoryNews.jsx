5a0529b201bab003b910ca0858493a49346d5133
Shaikh Kingshuk <shaikhkingshuk@gmail.com>
2025-10-21 22:21:19 +0600

    loading data using useEffect, based on 'id' that got from the params

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
   const { id } = useParams();
   const data = useLoaderData();
-  console.log(data);
-  console.log(id);
+  // console.log(data);
+  // console.log(id);
+  useEffect(() => {
+    const filteredNews = data.filter((news) => news.category_id == id);
+    console.log(filteredNews);
+  }, [id, data]);
+
   return <div>CategoryNews</div>;
 };
 
