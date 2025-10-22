317cdb27b23cfc6e00693ff9bc89835ae6ad6024
Shaikh Kingshuk <shaikhkingshuk@gmail.com>
2025-10-22 22:29:23 +0600

    observer function added

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,10 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
 
@@ -15,6 +19,15 @@ const AuthProvider = ({ children }) => {
   /// this function is mainly created to attach "auth" in "createUserWithEmailAndPassword"-function
   /// along with 'email & password'-which they get from the register page
   /// and to get register data, we send above function with the context
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const authData = {
     user,
