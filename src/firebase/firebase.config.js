1c8cc3a042337a4543d69451df66bd8ef443ec89
Shaikh Kingshuk <shaikhkingshuk@gmail.com>
2025-10-22 18:41:06 +0600

    setup firebase in project, also added email ang google provider to firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDi_2JHT1aWK5QuuBbDjjwtyPbrvlPIjzk",
+  authDomain: "dragon-news-auth-b46eb.firebaseapp.com",
+  projectId: "dragon-news-auth-b46eb",
+  storageBucket: "dragon-news-auth-b46eb.firebasestorage.app",
+  messagingSenderId: "718104003933",
+  appId: "1:718104003933:web:790b804aabf234cb1414b4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
