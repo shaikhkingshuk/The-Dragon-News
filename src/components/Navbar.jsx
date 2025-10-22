591a06f7ab79bef45562e07bbaa3299a0d37ce42
Shaikh Kingshuk <shaikhkingshuk@gmail.com>
2025-10-22 21:09:06 +0600

    dummy data created and send through 'AuthContext' to check it works correctly or not

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { use } from "react";
 import { Link, NavLink } from "react-router";
-import user from "../assets/user.png";
+import userImg from "../assets/user.png";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Navbar = () => {
+  const { user } = use(AuthContext);
   return (
     <div className="flex flex-row justify-between items-center">
-      <div></div>
+      <div>{user && user.email}</div>
       <div className="flex gap-5 text-accent">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/career">Career</NavLink>
       </div>
       <div className="flex flex-row gap-5">
-        <img src={user}></img>
+        <img src={userImg}></img>
         <Link to="/auth/login" className="btn btn-primary px-7">
           Login
         </Link>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,9 +1,17 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-const AuthProvider = () => {
-  return <AuthContext></AuthContext>;
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    name: "hablu mia",
+    email: "hablue@gmail.com",
+  });
+  const authData = {
+    user,
+    setUser,
+  };
+  return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
